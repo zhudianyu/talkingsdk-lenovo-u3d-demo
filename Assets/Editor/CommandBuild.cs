f61484d9f7e96bda5d65d962143f54e0f5346648
zhudianyu <dianyu.zhu@zqgame.com>
2015-04-23 10:28:15 +0800

    add 3d demo for lenovo

---
+++ Assets/Editor/CommandBuild.cs
@@ -0,0 +1,31 @@
+﻿/**
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -quit \
+  -projectPath $PROJECT_PATH \
+  -executeMethod CommandBuild.BuildAndroid
+*/
+
+// Assets/Editor/CommandBuile.cs
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+public class CommandBuild
+{
+	static List<string> levels = new List<string>();
+	public static void BuildAndroid()
+	{
+		foreach ( EditorBuildSettingsScene scene in EditorBuildSettings.scenes ) {
+			if ( !scene.enabled ) continue;
+			levels.Add( scene.path );
+		}
+		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.Android);
+        string[] scenes = { "Assets/aaa.unity" };
+		string res = BuildPipeline.BuildPlayer(scenes, "TalkingSdkU3dDemo.apk", BuildTarget.Android, BuildOptions.None );
+		if (res.Length > 0)
+			throw new Exception("BuildPlayer failure: " + res);
+	}
+}
